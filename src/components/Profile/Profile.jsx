5ea77f055aa8e007a1cbe893ff0f9a5d4b66af82
revut <revutskyg@gmail.com>
2021-12-07 20:24:28 +0200

    module

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,27 +1,27 @@
-import "./Profile.css";
+import s from "./Profile.module.css";
 
 const Profile = ({ user }) => {
   const { avatar, username, tag, location, stats } = user;
   return (
-    <div className="Profile-wrap">
-      <div className="description">
-        <img src={avatar} alt="User avatar" className="avatar" />
-        <p className="name">{username}</p>
-        <p className="tag colorTitle">{"@" + tag}</p>
-        <p className="location colorTitle">{location}</p>
+    <div className={s.Profilewrap}>
+      <div className={s.description}>
+        <img src={avatar} alt="User avatar" className={s.avatar} />
+        <p className={s.name}>{username}</p>
+        <p className={s.colorTitle}>{"@" + tag}</p>
+        <p className={s.colorTitle}>{location}</p>
       </div>
 
-      <ul className="stats">
-        <li className="Profile-item">
-          <span className="label colorTitle">Followers</span>
+      <ul className={s.stats}>
+        <li className={s.Profileitem}>
+          <span className={(s.label, s.colorTitle)}>Followers</span>
           <span className="quantity"> {stats.followers}</span>
         </li>
-        <li className="Profile-item">
-          <span className="label colorTitle">Views</span>
+        <li className={s.Profileitem}>
+          <span className={(s.label, s.colorTitle)}>Views</span>
           <span className="quantity"> {stats.views}</span>
         </li>
-        <li className="Profile-item">
-          <span className="label colorTitle">Likes</span>
+        <li className={s.Profileitem}>
+          <span className={(s.label, s.colorTitle)}>Likes</span>
           <span className="quantity"> {stats.likes}</span>
         </li>
       </ul>
